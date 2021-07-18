07e94c3e8a5251bbdd614410457d3490c77da871
Kelvin S. do Prado <kelvinpfw@gmail.com>
2021-07-18 17:30:42 -0300

    Update spotify.sh

--- arch/AUR/spotify.sh
+++ arch/AUR/spotify.sh
@@ -2,4 +2,7 @@
 
 curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | gpg --import -
 git clone https://aur.archlinux.org/spotify.git
+cd spotify
 makepkg -si --noconfirm
+cd ..
+rm -rf spotify

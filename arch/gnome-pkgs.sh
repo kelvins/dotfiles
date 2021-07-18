043461c75f81d8122304baa86934aa6e10ee79a6
Kelvin S. do Prado <kelvinpfw@gmail.com>
2021-07-18 16:44:33 -0300

    Create gnome-pkgs.sh

---
+++ arch/gnome-pkgs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+pacman -S \
+       baobab \
+       cheese \
+       eog \
+       evince \
+       file-roller \
+       gnome-backgrounds \
+       gnome-calculator \
+       sushi \
+       gnome-color-manager \
+       gnome-control-center \
+       gnome-disk-utility \
+       xdg-user-dirs-gtk \
+       mutter \
+       nautilus \
+       gnome-font-viewer \
+       gnome-keyring \
+       gnome-menus \
+       gnome-screenshot \
+       gnome-session \
+       gnome-settings-daemon \
+       gnome-shell \
+       gnome-shell-extensions \
+       gnome-themes-extra \
+       gnome-tweaks \
+       adwaita-icon-theme

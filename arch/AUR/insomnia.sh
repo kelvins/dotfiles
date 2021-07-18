6a09ac36b7b0b02d03056b3b27ec83ee215327dc
Kelvin S. do Prado <kelvinpfw@gmail.com>
2021-07-18 17:53:58 -0300

    Update insomnia.sh

--- arch/AUR/insomnia.sh
+++ arch/AUR/insomnia.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+sudo pacman -Syu --noconfirm npm
+
+git clone https://aur.archlinux.org/nvm.git
+cd nvm
+makepkg -si --noconfirm
+cd ..
+rm -rf nvm
+
 git clone https://aur.archlinux.org/insomnia.git
 cd insomnia
 makepkg -si --noconfirm

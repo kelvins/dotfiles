c757f86141bc1635bb06dc21087b0880c49bc972
Kelvin S. do Prado <kelvinpfw@gmail.com>
2022-04-05 22:52:05 -0300

    Update install.sh

--- osx/install.sh
+++ osx/install.sh
@@ -5,53 +5,14 @@ sudo xcode-select --install
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew doctor
 
-# Install git
-brew install git
-
-# Install iTerm2
 brew install --cask iterm2
-
-# Install ZSH
 brew install zsh
 
 # Install oh-my-zsh
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-# Install vim
-brew install vim
+brew install vim htop ack tree curl httpie
 
-# Install htop
-brew install htop
+brew install pyenv rustup go
 
-# Install ack
-brew install ack
-
-# Install tree
-brew install tree
-
-# Install curl
-brew install curl
-
-# Install httpie
-brew install httpie
-
-# Install pyenv
-brew install pyenv
-
-# Install Rust
-brew install rustup
-
-# Install Go
-brew install go
-
-# Install kubectl
-brew install kubectl
-
-# Install helm
-brew install helm
-
-# Install kustomize
-brew install kustomize
-
-# Install K9S
-brew install derailed/k9s/k9s
+brew install kubectl helm kustomize k9s

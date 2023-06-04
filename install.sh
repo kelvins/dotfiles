ed750f928f9324f6dfa953a2dc3d5ded409ade91
Kelvin S. do Prado <kelvinpfw@gmail.com>
2023-06-04 17:45:37 -0300

    Update install.sh script

--- install.sh
+++ install.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
 echo "Updating package index..."
-sudo apt update
+sudo apt -qq update
 
 echo "Installing essentials..."
+
 echo "  [build-essential] Meta-package to build dependencies"
-apt install -y build-essential
+sudo apt -qq install -y build-essential
+
+echo "  [libssl-dev] Dev libraries for SSL/TLS"
+sudo apt -qq install -y libssl-dev
+
+echo "  [openssl] Library used for network communication"
+sudo apt -qq install -y openssl
+
+echo "  [curl] Client URL"
+sudo apt -qq install -y curl

92291ef86e957ea3658063571642469098550923
Kelvin S. do Prado <kelvinpfw@gmail.com>
2023-10-26 10:46:59 -0300

    Update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -67,6 +67,15 @@
   :init (doom-modeline-mode 1)
   :custom ((doom-modeline-height 20)))
 
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 0.3))
+
 (use-package dracula-theme)
 
 (load-theme 'dracula t)

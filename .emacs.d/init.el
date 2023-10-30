2966fad52ddfeca0e00ed5348abc54ae692c913b
Kelvin S. do Prado <kelvinpfw@gmail.com>
2023-10-30 09:04:54 -0300

    Update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,14 +1,1 @@
 (org-babel-load-file "~/.emacs.d/config.org")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(flycheck all-the-icons which-key visual-fill-column scala-mode rainbow-delimiters org-bullets neotree ivy-rich evil elpy dracula-theme doom-modeline dashboard command-log-mode cider)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )

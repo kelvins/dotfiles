f4f90bdaf179035f09a4a9393c0980083ba12dd8
Kelvin S. do Prado <kelvinpfw@gmail.com>
2024-07-29 14:03:31 -0300

    Add home-manager

--- nix/flake.nix
+++ nix/flake.nix
@@ -2,17 +2,31 @@
   description = "My NixOS Flake!";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-24.05";
+    nixpkgs = {
+      url = "nixpkgs/nixos-24.05";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }:
+  outputs = { self, nixpkgs, home-manager, ... }:
     let
       lib = nixpkgs.lib;
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
     in {
     nixosConfigurations = {
       kelvins = lib.nixosSystem {
-        system = "x86_64-linux";
+        inherit system;
         modules = [ ./configuration.nix ];
+      };
+    };
+    homeConfigurations = {
+      kelvins = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ ./home.nix ];
       };
     };
   };

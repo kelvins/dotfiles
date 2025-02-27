7a98f01562db138a1a4538bd8b445b816d9255f5
Kelvin Prado <kelvin.prado@nubank.com.br>
2025-02-27 14:56:57 -0300

    Add telescope

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -9,3 +9,6 @@ map('n', '<C-j>', '<C-w>j', { noremap = true, silent = true }) -- Down
 map('n', '<C-k>', '<C-w>k', { noremap = true, silent = true }) -- Up
 map('n', '<C-l>', '<C-w>l', { noremap = true, silent = true }) -- Right
 
+local builtin = require('telescope.builtin')
+map('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+map('n', '<leader>fg', builtin.live_grep,  { desc = 'Telescope live grep'  })
--- .config/nvim/lua/plugins/dashboard.lua
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -1,9 +1,43 @@
+local header = {
+  ' ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗',
+  ' ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║',
+  ' ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║',
+  ' ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║',
+  ' ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║',
+  ' ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝',
+  '',
+}
+
 return {
   'nvimdev/dashboard-nvim',
   event = 'VimEnter',
   config = function()
     require('dashboard').setup {
-      theme = 'hyper'
+      theme = 'hyper',
+      config = {
+        header = header,
+        shortcut = {
+          {
+            key = 'u',
+            desc = 'Update',
+            group = '@property',
+            action = 'Lazy update'
+          },
+          {
+            key = 'f',
+            desc = 'Files',
+            group = '@property',
+            action = 'Telescope find_files'
+          },
+          {
+            key = 'q',
+            desc = 'Quit',
+            group = '@property',
+            action = 'quit'
+          }
+        },
+        footer = {}
+      }
     }
   end,
   dependencies = { {'nvim-tree/nvim-web-devicons'}}

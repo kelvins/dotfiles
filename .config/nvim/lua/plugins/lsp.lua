6337e553442fad3c770749fc14c05451d4b02709
Kelvin S. do Prado <kelvinpfw@gmail.com>
2025-03-06 20:57:38 -0300

    LSP config neovim

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,12 +1,13 @@
 return {
   "neovim/nvim-lspconfig",
   config = function()
-    require("lspconfig").ruff.setup {}
-    require("lspconfig").gopls.setup {}
-    require("lspconfig").lua_ls.setup {}
-    require("lspconfig").metals.setup {}
-    require("lspconfig").pyright.setup {}
-    require("lspconfig").clojure_lsp.setup {}
-    require("lspconfig").rust_analyzer.setup {}
+    local lspconfig = require('lspconfig')
+    lspconfig.ruff.setup {}
+    lspconfig.gopls.setup {}
+    lspconfig.lua_ls.setup {}
+    lspconfig.metals.setup {}
+    lspconfig.pyright.setup {}
+    lspconfig.clojure_lsp.setup {}
+    lspconfig.rust_analyzer.setup {}
   end,
 }

fdc7598d1a7befceed39ab7fe015c7e5d2fdcf1e
Kelvin S. do Prado <kelvinpfw@gmail.com>
2025-03-06 21:13:09 -0300

    Neovim LSP mappings

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -12,3 +12,23 @@ map('n', '<C-l>', '<C-w>l', { noremap = true, silent = true }) -- Right
 local builtin = require('telescope.builtin')
 map('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 map('n', '<leader>fg', builtin.live_grep,  { desc = 'Telescope live grep'  })
+
+
+-- This is where you enable features that only work if there is a language server active in the file
+vim.api.nvim_create_autocmd('LspAttach', {
+  desc = 'LSP actions',
+  callback = function(event)
+    local opts = {buffer = event.buf}
+
+    map('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+    map('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+    map('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+    map('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+    map('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+    map('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+    map('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+    map('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+    map({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+    map('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+  end,
+})

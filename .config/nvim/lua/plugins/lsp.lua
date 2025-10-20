28fa92a9c5ed311609fc5bf53fe32105a7a00a29
Kelvin Prado <kelvin.prado@nubank.com.br>
2025-10-20 11:19:38 -0300

    Update nvim LSP config

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,36 +1,44 @@
 return {
   'neovim/nvim-lspconfig',
   config = function()
-    local lspconfig = require('lspconfig')
+    -- Use Neovim 0.11+ API (see :help lspconfig-nvim-0.11)
+
     -- Python
-    vim.lsp.config('ruff', {})
-    --lspconfig.pylsp.setup {}
+    vim.lsp.enable('ruff')
+    -- If you also use pylsp, enable it similarly:
+    -- vim.lsp.enable('pylsp')
+
     -- Go
-    vim.lsp.config('gopls', {})
-    -- Lua
+    vim.lsp.enable('gopls')
+
+    -- Lua (keep the custom settings)
     vim.lsp.config('lua_ls', {
       settings = {
         Lua = {
           runtime = {
-            version = 'LuaJIT'
+            version = 'LuaJIT',
           },
           diagnostics = {
-            globals = { 'vim', 'require' }
+            globals = { 'vim', 'require' },
           },
           workspace = {
-            library = vim.api.nvim_get_runtime_file('', true)
+            library = vim.api.nvim_get_runtime_file('', true),
           },
           telemetry = {
-            enable = false
-          }
-        }
-      }
+            enable = false,
+          },
+        },
+      },
     })
+    vim.lsp.enable('lua_ls')
+
     -- Scala
-    vim.lsp.config('metals', {})
+    vim.lsp.enable('metals')
+
     -- Clojure
-    vim.lsp.config('clojure_lsp', {})
+    vim.lsp.enable('clojure_lsp')
+
     -- Rust
-    vim.lsp.config('rust_analyzer', {})
+    vim.lsp.enable('rust_analyzer')
   end,
 }
